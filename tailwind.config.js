59aa4f3c2978ec89655dfac4f76835ba23a75d17
Pete Thongnate <pete@cnx.movaci.net>
2025-06-25 13:18:41 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+// tailwind.config.js
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        yellow: 'hsl(47, 88%, 63%)',
+        white: 'hsl(0, 0%, 100%)',
+        gray: {
+          500: 'hsl(0, 0%, 42%)',
+          950: 'hsl(0, 0%, 7%)',
+        },
+      },
+    },
+     fontFamily: {
+        figtree: ['Figtree', 'sans-serif'],
+      },
+  },
+}
